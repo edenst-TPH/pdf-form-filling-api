1332c54c12153e7e0d0370ce79fd291efd349e15
tertek <ekin.tertemiz@swisstph.ch>
2024-03-05 09:58:34 +0100

    fix too many flags, certbot

--- .docker/nginx/entrypoint-nginx.sh
+++ .docker/nginx/entrypoint-nginx.sh
@@ -8,11 +8,11 @@ echo $APP_ENVIRONMENT
 
 # certbot certonly --webroot -w /var/www/html -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos
 
-certbot --webroot -w /var/www/html --nginx -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos
+certbot --nginx -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos
 
-mkdir -p /etc/letsencrypt/ssl
-cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/fullchain.pem /etc/letsencrypt/ssl/
-cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/privkey.pem /etc/letsencrypt/ssl/
+# mkdir -p /etc/letsencrypt/ssl
+# cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/fullchain.pem /etc/letsencrypt/ssl/
+# cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/privkey.pem /etc/letsencrypt/ssl/
 
 echo "starting nginx"
 
