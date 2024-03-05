2fdfdb3df2cd3f1bd2ce7c8dd4312e9aca18b3dc
tertek <ekin.tertemiz@swisstph.ch>
2024-03-05 10:58:39 +0100

    add fullpaths

--- .docker/nginx/entrypoint-nginx.sh
+++ .docker/nginx/entrypoint-nginx.sh
@@ -8,9 +8,9 @@ echo "installing certificates"
 
 certbot certonly --standalone -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos
 
-mkdir -p /etc/letsencrypt/ssl
-cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/fullchain.pem /etc/letsencrypt/ssl/
-cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/privkey.pem /etc/letsencrypt/ssl/
+# mkdir -p /etc/letsencrypt/ssl
+# cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/fullchain.pem /etc/letsencrypt/ssl/
+# cp -r -L /etc/letsencrypt/live/143.198.242.211.sslip.io/privkey.pem /etc/letsencrypt/ssl/
 
 echo "starting nginx"
 
