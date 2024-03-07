7b65e44e2e7b5c9392da2a76b2200fca1e32c2c6
tertek <ekin.tertemiz@swisstph.ch>
2024-03-07 09:30:30 +0100

    fix symlink resolution

--- bin/create-cert.sh
+++ bin/create-cert.sh
@@ -4,6 +4,9 @@ if [ "$1" == "--run" ]
 then 
     echo "[RUN]"
     docker compose -f compose.dev.yml run --rm certbot certonly --webroot -w /var/www/certbot/ -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos --non-interactive 
+
+    cp -Lr .docker/certbot/conf/live/ .docker/nginx/ssl/
+
 else
     echo "[DRY RUN]"
     docker compose -f compose.dev.yml run --rm certbot certonly --webroot -w /var/www/certbot/ -d 143.198.242.211.sslip.io -d www.143.198.242.211.sslip.io --email ekin.tertemiz@swisstph.ch --agree-tos --non-interactive --dry-run
