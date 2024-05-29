4eb5757450383ffccc2eba629791d7e3c01ab46a
tertek <ekin.tertemiz@swisstph.ch>
2024-05-29 16:27:11 +0200

    Fix middleware order.

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -7,12 +7,13 @@ use Slim\App;
 use Slim\Views\TwigMiddleware;
 use Middlewares\TrailingSlash;
 
+
 return function (App $app) {
-    $app->add((new TrailingSlash(true))->redirect());
     $app->addBodyParsingMiddleware();
     $app->add(TwigMiddleware::class);
     $app->add(ValidationMiddleware::class);
     $app->addRoutingMiddleware();
     $app->add(BasePathMiddleware::class);
     $app->add(ExceptionMiddleware::class);
+    $app->add((new TrailingSlash(true))->redirect());
 };
