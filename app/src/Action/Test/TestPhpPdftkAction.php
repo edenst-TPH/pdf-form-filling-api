bc6389fd0f796f6f0091ca7507e9d41820339f5c
tertek <ekin.tertemiz@swisstph.ch>
2024-07-04 08:36:58 +0200

    Add example on getting fields from stored file.

--- app/src/Action/Test/TestPhpPdftkAction.php
+++ app/src/Action/Test/TestPhpPdftkAction.php
@@ -5,12 +5,23 @@ namespace App\Action\Test;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use mikehaertl\pdftk\Pdf;
+use App\Filesystem\Storage;
 
 final class TestPhpPdftkAction {
 
+    private Storage $storage;
+
+    public function __construct(Storage $storage) 
+    {
+        $this->storage = $storage;
+    }
+
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $pdf = new Pdf('pdftk_test_document.pdf');
+        // Get Data Fields from file in storage
+        $path = $this->storage->read("/foo/b7a1fafe9765518d.pdf");
+        $pdf = new Pdf($path);
+
         $result = $pdf->getDataFields();
 
         $response->getBody()->write(json_encode($result));
